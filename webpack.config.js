147e1eb875a63575c47dafb968a1e18eda57b8c1
Ana Beatriz <anabiawalker@gmail.com>
2022-01-14 15:51:41 -0300

    Adicionando pasta de saída

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,9 @@ const webpack = require('webpack')
 module.exports = {
     //Modo de desenvolvimento
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    }
 }
